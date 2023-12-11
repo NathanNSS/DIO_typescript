421b495ea571795a74f27c10b2a1927fc1f466be
Nathan Soares dos Santos <nathannss14@gmail.com>
2023-12-11 12:06:52 -0300

    concluida

--- app.ts
+++ app.ts
@@ -2,22 +2,20 @@
 let arr = [1,2,3] //[1,2,3] 
 const arr_ = new Array(1,2,3) //[1,2,3] !(e possivel construir o array vazios com espaços vazios assim: new Array(3) >> [undefined, undefined,undefined])
 
-//Forma de tipagem dos array
-const arr1: number[] = [1,2,3,4,5] 
-const arr2: string[] = ["1","2","3","4",'5']
 
 //Alguns dos metodos do array
 
-arr1.push(6) //Metodo push adiciona um item ao final do array 
-arr1.pop() //Metodo pop remove um item ao final do array
+let res_arr = arr.find(num => num >= 2)//O metodo find() retorna o valor do primeiro elemento do array que satisfizer a função de teste provida. Caso contrario, undefined é retornado.
+res_arr = arr.findIndex(num => num >= 2)//O método findIndex() retorna o índice no array do primeiro elemento que satisfizer a função de teste provida. Caso contrário, retorna -1, indicando que nenhum elemento passou no teste.
 
-arr1.unshift(0) //Metodo unshift adiciona um item ao inicio do array
-arr1.shift() //Metodo shift remove um item ao inicio do array
+arr.forEach(num => {
+    if(num >= 2) res_arr = num
+})//O metodo forEach() executa uma dada função em cada elemento de um array. Não possue retorno!
+
+arr.map(num => {
+    if(num >= 2) console.log(num)
+})//O método map() invoca a função callback passada por argumento para cada elemento do Array e devolve um novo Array como resultado. map sempre retorna um "novo" array com o mesmo comprimento do original!
 
 
-//Uma das propiedade do array
 
-arr1.length //Uma propiedade do array que recebe seu tamanho
-
-
-console.log(arr1)
+console.log(res_arr)
