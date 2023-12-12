252880f4f679915d2596f8ab0d4b6b3894e8014f
Nathan Soares dos Santos <nathannss14@gmail.com>
2023-12-12 11:12:08 -0300

    concluida

--- app.ts
+++ app.ts
@@ -6,3 +6,4 @@ const account2: CompanyAccount = new CompanyAccount("Nathan", 123456)
 
 console.log(account)
 console.log(account2)
+account2.deposit()
--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,18 +1,36 @@
 export abstract class DioAccount {
-    name: string
+    private name: string
     accountNumber: number
     balance: number = 0
+    status:boolean = true
 
     constructor(name: string, accountNumber: number) {
         this.name = name
         this.accountNumber = accountNumber
     }
 
+    getName(): string {
+        return this.name
+    }
+
+    setName(name: string) {
+        this.name = name
+    }
+
     deposit() {
+        if(this.validateAccount())
         console.log("Voce fez um deposito na conta " + this.accountNumber)
     }
 
     withDraw() {
         console.log("Voce fez um saque na conta " + this.accountNumber)
+    }
+
+    private validateAccount(){
+        if(this.status){
+            return this.status
+        }
+
+        throw new Error("Conta invalida :(")
     }
 }
