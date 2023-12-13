3c17df9db8ab1c159ed1127b3e30ddafb62ff0a4
Nathan Soares dos Santos <nathannss14@gmail.com>
2023-12-13 10:31:05 -0300

    concluida

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+
+export function Card() {
+    return (
+        <>
+            <fieldset style={{ width: 'fit-content' }}>
+                <h3>Title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, similique suscipit.</p>
+            </fieldset>
+            <br />
+        </>
+    )
+}
